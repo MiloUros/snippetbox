28df852c9f1bbfb4672e62f08c6436a67edb2980
uromilo <milovanovicuros995@gmail.com>
2024-08-31 17:51:42 +0200

    Removed routs from main, created snippets.go to preform queries on the db.

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -5,12 +5,12 @@ import "net/http"
 func (app *application) routes() *http.ServeMux {
 	mux := http.NewServeMux()
 
-	fileServer := http.FileServer(http.Dir("./ui/static"))
-	mux.Handle("/static/", http.StripPrefix("/static/", fileServer))
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-	mux.HandleFunc("/", app.home)
 	mux.HandleFunc("/snippet/view", app.snippetView)
-	mux.HandleFunc("snippet/create", app.snippetCreate)
-
+	mux.HandleFunc("/snippet/create", app.snippetCreate)
+	mux.HandleFunc("/snippet/create/", app.snippetCreate)
+	mux.HandleFunc("/", app.home)
 	return mux
 }
